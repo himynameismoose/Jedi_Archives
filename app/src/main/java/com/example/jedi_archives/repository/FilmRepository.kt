b20cd22e99c31d22c418dfc176a11d5da4477267
Mershelle Rivera <mrivera21@mail.greenriver.edu>
2022-03-14 15:23:12 -0700

    Create repo to fetch data

---
+++ app/src/main/java/com/example/jedi_archives/repository/FilmRepository.kt
@@ -0,0 +1,16 @@
+package com.example.jedi_archives.repository
+
+import com.apollographql.apollo.api.Response
+import com.example.jedi_archives.AllFilmsQuery
+
+/**
+ * This interface will create FilmRepositoryImpl
+ * This will be used to query AllFilmsQuery.kt
+ *
+ * @author Mershelle
+ * @version 1.0
+ */
+interface FilmRepository {
+
+    suspend fun queryFilmList(): Response<AllFilmsQuery.Data>
+}
