0ce9b8d7b98d7c8e5f86bdd8e515cf8709052d3e
Mershelle Rivera <mrivera21@mail.greenriver.edu>
2022-03-15 12:18:51 -0700

    Add module and repo implementation

--- app/src/main/java/com/example/jedi_archives/repository/FilmRepositoryImpl.kt
+++ app/src/main/java/com/example/jedi_archives/repository/FilmRepositoryImpl.kt
@@ -1,10 +1,15 @@
 package com.example.jedi_archives.repository
 
 import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.coroutines.await
 import com.example.jedi_archives.AllFilmsListQuery
+import com.example.jedi_archives.networking.JediArchivesApi
+import javax.inject.Inject
 
-class FilmRepositoryImpl : FilmRepository {
+class FilmRepositoryImpl @Inject constructor(
+    private val webService: JediArchivesApi
+) : FilmRepository {
     override suspend fun queryAllFilmsList(): Response<AllFilmsListQuery.Data> {
-        TODO("Not yet implemented")
+        return webService.getApolloClient().query(AllFilmsListQuery()).await()
     }
 }
