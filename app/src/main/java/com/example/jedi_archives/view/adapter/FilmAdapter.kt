2087765bcd53fb9c4e4d9b041cf4ecbe1ba93b48
Mershelle Rivera <mrivera21@mail.greenriver.edu>
2022-03-14 20:03:58 -0700

    Update adapter implementation

--- app/src/main/java/com/example/jedi_archives/view/adapter/FilmAdapter.kt
+++ app/src/main/java/com/example/jedi_archives/view/adapter/FilmAdapter.kt
@@ -1,12 +1,30 @@
 package com.example.jedi_archives.view.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.jedi_archives.FilmListQuery
+import com.example.jedi_archives.R
 import com.example.jedi_archives.databinding.ItemFilmBinding
 
-class FilmAdapter {
+class FilmAdapter : ListAdapter<FilmListQuery.Film, FilmViewHolder>(FilmDiffUtil()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FilmViewHolder {
+        val binding: ItemFilmBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_film,
+            parent,
+            false
+        )
+
+        return FilmViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: FilmViewHolder, position: Int) {
+        holder.binding.film = getItem(position)
+    }
 
 }
 
